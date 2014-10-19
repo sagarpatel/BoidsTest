102e9abf97dfa5f57d9ea68251b11e898fd50ca5
Sagar Patel <17.sagar.patel@gmail.com>
2014-10-19 16:19:37 +0900

    Basic Unity scene with custom particle system working

---
+++ BoidsTestUnityProjectFolder/BoidsTestUnityProject/Assets/Scripts/ParticleBoids.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleBoids : MonoBehaviour 
+{
+	ParticleSystem.Particle[] particlesArray;
+	public int particlesCount = 100;
+
+	void Start () 
+	{
+
+		particlesArray = new ParticleSystem.Particle[particlesCount];
+		for(int i = 0; i < particlesArray.Length; i++)
+		{
+			particlesArray[i].color = Color.white;
+			particlesArray[i].position = 10.0f * Random.onUnitSphere;
+			particlesArray[i].size = 1.0f;
+		}
+		particleSystem.SetParticles(particlesArray,particlesArray.Length);
+	
+	}
+
+		
+
+
+	void Update () 
+	{
+	
+	}
+}
